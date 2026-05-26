5c43e7d17b7a75e3b2aa0364007c6f8a6d22e9f2
Fireflies3072 <cxu4096@outlook.com>
2026-05-26 11:06:12 -0500

    Add __init__ for flow sampler

--- ff/nn/__init__.py
+++ ff/nn/__init__.py
@@ -1,9 +1,10 @@
-from .architecture import *
 from .ema import *
 from .utils import *
 from .functional import *
 from .perceptual_loss import *
 from .vae_processor import *
 
+from .architecture import *
 from .diffusion import *
 from .dataset import *
+from .flow import *
