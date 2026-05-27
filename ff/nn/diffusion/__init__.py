5d9515c3e7a873db577c9a8d755deff905179a41
Fireflies3072 <cxu4096@outlook.com>
2026-05-27 13:50:10 -0500

    Import fix

--- ff/nn/diffusion/__init__.py
+++ ff/nn/diffusion/__init__.py
@@ -3,7 +3,6 @@ from .scheduler import LinearScheduler, CosineScheduler
 from .sampler import *
 from .unet import UnconditionalUNet, ClassConditionalUNet
 from .dit import (
-    UnconditionalDiT1d, UnconditionalDiT2d, UnconditionalDiT3d,
-    ClassConditionalDiT1d, ClassConditionalDiT2d, ClassConditionalDiT3d,
+    DiT1d, DiT2d, DiT3d,
     ContextConditionalDiT1d, ContextConditionalDiT2d, ContextConditionalDiT3d
 )
