71ac680c8e8a8fe0dc2d60f9144ef1c88cd9bd30
Fireflies3072 <cxu4096@outlook.com>
2026-05-28 16:16:47 -0500

    Fix dataset bug imagenet

--- ff/nn/dataset/ops.py
+++ ff/nn/dataset/ops.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import random
 import cv2
-from collections.abc import Sequence
+from collections.abc import Sequence, Callable
 
 def to_tensor(data: np.ndarray|torch.Tensor) -> torch.Tensor:
     if isinstance(data, np.ndarray):
@@ -45,10 +45,12 @@ def rescale_unit(data: torch.Tensor) -> torch.Tensor:
 def rescale_signed(data: torch.Tensor) -> torch.Tensor:
     return data * 2.0 - 1.0
 
-def create_normalizer(mean: Sequence[float], std: Sequence[float]) -> callable:
+def create_normalizer(mean: Sequence[float], std: Sequence[float]) -> Callable[[torch.Tensor], torch.Tensor]:
     mean_tensor = torch.tensor(mean).view(-1, 1, 1)
     std_tensor = torch.tensor(std).view(-1, 1, 1)
-    return lambda x: (x - mean_tensor) / std_tensor
+    def rescale(x: torch.Tensor) -> torch.Tensor:
+        return (x - mean_tensor) / std_tensor
+    return rescale
 
 def image_to_signed(data: np.ndarray) -> torch.Tensor:
     return torch.from_numpy(data).to(dtype=torch.float32).permute(2, 0, 1) / 127.5 - 1.0
